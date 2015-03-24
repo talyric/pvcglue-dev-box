cdf070f107e01aafc5967d2c3a11106c6a423adb
Andrew Lyric <talyric@gmail.com>
2015-03-24 08:42:03 -0700

    secrets

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 if Rails.env.production?
-  Store::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN'] || raise('No secret token specified.  :(')
+  Rails.application.config.secret_token = ENV['RAILS_SECRET_TOKEN'] || raise('No secret token specified.  :(')
 else
-  Store::Application.config.secret_token = 'f4e042f35beb865aaf6db0c3bd539999e1cfdf6402b0bebbcdbd67fa7d4e02ea1a2ecefde033c3cd935569ec5ecc106fc451fd789ffec07a338ea810f0b85778'
+  Rails.application.config.secret_token = 'f4e042f35beb865aaf6db0c3bd539999e1cfdf6402b0bebbcdbd67fa7d4e02ea1a2ecefde033c3cd935569ec5ecc106fc451fd789ffec07a338ea810f0b85778'
 end
