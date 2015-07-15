05fe541d3d59b1417cc8478c66d9c21491223300
Andrew Lyric <talyric@gmail.com>
2015-07-15 10:46:49 -0700

    monit

--- app/controllers/monit_controller.rb
+++ app/controllers/monit_controller.rb
@@ -1,8 +1,12 @@
 class MonitController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @result = MonitWeb.new.call(request.env)
     # puts request.env.inspect
     # render :status => 200, :content_type => @result[1]["content-type"], :layout => false, :text => @result[2]
-    render :status => 200, :layout => false, :text => @result[2]
+    text = @result[2].to_s
+    text.gsub!(/href='/, "/href='monit/")
+    render :status => 200, :layout => false, :text => text
   end
 end
